7886c71db34401892c381b785f31e901497921fb
Muhammad Azzam <mazzamabduljabbar@gmail.com>
2019-01-31 12:19:00 +0700

    #4 Fixing typo

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ app.get('/values/current', async(req, res) => {
 
 app.post('/values', async(req, res) => {
     const index = req.body.index;
-    if (parseInt(Index) > 40) {
+    if (parseInt(index) > 40) {
         return res.status(422).send('Index too high');
     }
 
